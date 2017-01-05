816b4011653445150bb0164b8ea30150b0db6c42
Thomas Broyer <t.broyer@ltgt.net>
2017-01-05 15:17:17 +0100

    net/http/testhandlers: add documentation

--- net/http/testhandlers/testhandlers.go
+++ net/http/testhandlers/testhandlers.go
@@ -2,6 +2,10 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+// Package testhandlers provides http handlers that can be useful for setting
+// up test servers (most likely with a net/http/httptest.Server).
+//
+// They're not meant to be used in production.
 package testhandlers // import "go.ltgt.net/net/http/testhandlers"
 
 import (
@@ -12,6 +16,12 @@ import (
 
 var sleep = time.Sleep
 
+// Delay wraps an http.Handler to delay it based on the request's query-string,
+// expecting a query parameter named 'delay' whose value is a duration (parsed
+// using time.ParseDuration).
+//
+// The wrapped handler will be called immediately in case the 'delay' query
+// parameter is absent or its value is malformed or negative.
 func Delay(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		q := r.URL.Query()
@@ -24,6 +34,12 @@ func Delay(h http.Handler) http.Handler {
 	})
 }
 
+// AddHeaders wraps an http.Handler to add response headers based on the
+// request's query-string, expecting query parameters named 'header' whose
+// value is an HTTP header line (header name and value separated by a colon.)
+//
+// The headers are added to the http.ResponseWriter before the wrapped handler
+// is called (so the handler can alter those headers.)
 func AddHeaders(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		q := r.URL.Query()
